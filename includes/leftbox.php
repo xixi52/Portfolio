315dff3358af1703ef81ecd939344788069b4b9b
Alexis Mourey <one.com.suspendu@gmail.com>
2022-09-27 20:53:23 +0200

    Update with php includes

--- includes/leftbox.php
+++ includes/leftbox.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Alexis Mourey</title>
-  <link rel="stylesheet" href="./assets/css/style.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-</head>
-<body>
-  <main>
-
-    <!-- DEBUT LEFTBOX -->
     <aside class="leftbox">
       <div class="leftbox-info">
         <div class="avatar">
@@ -86,14 +68,3 @@
         </ul>
       </div>
     </aside>
-    <!-- FIN LEFTBOX -->
-
-    <!-- DEBUT CONTENT -->
-    <div class="main-content">
-
-    </div>
-    <!-- FIN CONTENT -->
-
-  </main>
-</body>
-</html>
