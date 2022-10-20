df38565eb36c809c90093ee0caa25e408365d981
Alexis Mourey <51019403+xixi52@users.noreply.github.com>
2022-10-20 17:14:55 +0200

    Add projects

---
+++ assets/js/projects.js
@@ -0,0 +1,55 @@
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+const select = document.querySelector("[data-select]"),
+selectItems = document.querySelectorAll("[data-select-item]"),
+  selectValue = document.querySelector("[data-select-value]"),
+  filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "tous") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
--- projets.php
+++ projets.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Alexis Mourey</title>
   <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/projects.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
@@ -36,6 +37,75 @@
           <h2 class="h2 article-title">Projets</h2>
         </header>
 
+
+        <section class="projects">
+
+          <ul class="filter-list">
+            <li class="filter-item">
+              <button class="active" data-filter-btn="">Tous</button>
+            </li>
+            <li class="filter-item">
+              <button data-filter-btn="">Web</button>
+            </li>
+            <li class="filter-item">
+              <button data-filter-btn="">Modules</button>
+            </li>
+          </ul>
+
+          <div class="filter-select-box">
+
+            <button class="filter-select" data-select="">
+              <div class="select-value" data-select-value="">Tous</div>
+              <div class="select-icon">
+                <i class="fa-solid fa-chevron-down"></i>
+              </div>
+            </button>
+
+            <ul class="select-list">
+              <li class="select-item">
+                <button data-select-item="">Tous</button>
+              </li>
+              <li class="select-item">
+                <button data-select-item="">Web</button>
+              </li>
+              <li class="select-item">
+                <button data-select-item="">Modules</button>
+              </li>
+            </ul>
+          </div>
+
+          <ul class="project-list">
+
+          <li class="project-item active" data-filter-item="" data-category="web">
+              <a href="#">
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <i class="fa-regular fa-eye"></i>
+                  </div>
+                  <img src="assets/images/project-ipcalculator.png" alt="IP Calculator" loading="lazy">
+                </figure>
+                <h3 class="project-title">IP Calculator</h3>
+                <p class="project-category">Web</p>
+              </a>
+            </li>
+
+            <li class="project-item active" data-filter-item="" data-category="modules">
+              <a href="#">
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <i class="fa-regular fa-eye"></i>
+                  </div>
+                  <img src="assets/images/project-discordcanvas.png" alt="Discord Canvas" loading="lazy">
+                </figure>
+                <h3 class="project-title">Discord Canvas</h3>
+                <p class="project-category">Modules</p>
+              </a>
+            </li>
+
+          </ul>
+
+        </section>
+
       </article>
       <!-- FIN ARTICLE PAGE -->
 
@@ -53,6 +123,7 @@
   <!-- DEBUT SCRIPTS -->
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/script.js"></script>
+  <script src="assets/js/projects.js"></script>
   <!-- FIN SCRIPTS -->
 </body>
 </html>
